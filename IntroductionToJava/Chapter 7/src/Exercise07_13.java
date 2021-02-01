1a6bcbe6563104817441d0074a6f799a89d976a7
Turan <turan.ktas@gmail.com>
2021-02-01 21:31:44 +0300

    updated

--- IntroductionToJava/Chapter 7/src/Exercise07_13.java
+++ IntroductionToJava/Chapter 7/src/Exercise07_13.java
@@ -1,4 +1,20 @@
+import java.util.Arrays;
+public class Exercise07_13{
 
-public class Exercise07_13 {
+    public static void main(String args[]){
 
+        System.out.println(getRandom(1,2,3,4,5,6,7,8,9,10));
+    }
+
+    public static int getRandom(int... numbers){
+        int random;
+        int search;
+
+        do{
+            random = (int)(Math.random() * 54) + 1;
+            search = Arrays.binarySearch(numbers , random);
+        }while(search >= 0);
+
+        return random;
+    }
 }
