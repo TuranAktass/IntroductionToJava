fca6bf50aa8bf742ed920de631ec760d8c0eecc9
TuranAktass <turan.ktas@gmail.com>
2021-02-13 12:11:08 +0300

    updated

--- IntroductionToJava/Chapter 1/Exercise1.java
+++ IntroductionToJava/Chapter 1/Exercise1.java
@@ -1,9 +1,7 @@
 public class Exercise1{
     public static void main(String args[]){
         System.out.println("Welcome to Java");
-        System.out.println("Welcome to Computer Science");
-        
-        
+        System.out.println("Welcome to Computer Science");    
         System.out.println("Programming is Fun");
     }
 }
