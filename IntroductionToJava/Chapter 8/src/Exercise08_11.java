61f7918300e2f7a87fee546f3dde605be8c4ebfb
TuranAktass <turan.ktas@gmail.com>
2021-02-16 12:28:56 +0300

    updated

--- IntroductionToJava/Chapter 8/src/Exercise08_11.java
+++ IntroductionToJava/Chapter 8/src/Exercise08_11.java
@@ -2,15 +2,7 @@
 public class Exercise08_11 {
 	
 	public static void main(String args[]) {
-		
-		int [][] coins = new int[3][3];
-		char [][] chCoins = new char[3][3];
-		
-		for(int i = 0; i < coins.length; i++) {
-			for(int j = 0; j < coins[0].length; j++) {
-				coins[i][j] = (int)(Math.random() * 2);
-			}
-		}
+
 	}
 	
 	
