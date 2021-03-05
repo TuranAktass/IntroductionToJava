309486a7ef876e0e2bb5f42dc1f66603de366197
TuranAktass <turan.ktas@gmail.com>
2021-03-05 18:11:15 +0300

    updated

--- IntroductionToJava/Chapter09/E09_11/src/E09_11.java
+++ IntroductionToJava/Chapter09/E09_11/src/E09_11.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+
 public class E09_11 {
-	
+
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Enter a, b, c, d, e, f :");
 		double a = input.nextDouble();
 		double b = input.nextDouble();
@@ -11,16 +12,15 @@ public class E09_11 {
 		double d = input.nextDouble();
 		double e = input.nextDouble();
 		double f = input.nextDouble();
-		
-		LinearEquation eq1 = new LinearEquation(a,b,c,d,e,f);
-		
-		if(!eq1.isSolvable()) {
+
+		LinearEquation eq1 = new LinearEquation(a, b, c, d, e, f);
+
+		if (!eq1.isSolvable()) {
 			System.out.println("The equation has no solutions!");
-		}
-		else {
+		} else {
 			System.out.println("x : " + eq1.getX() + "\ny : " + eq1.getY());
 		}
-		
+
 		input.close();
 	}
 
